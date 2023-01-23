a295300c5fb67ad6a9afc8e8bfa564f30e0fffac
quaaalud <dludwins@outlook.com>
2023-01-23 17:04:53 -0600

    fixed path to image carousel

--- __helpers__/__image_carousel__.py
+++ __helpers__/__image_carousel__.py
@@ -24,7 +24,7 @@ def _return_url_bytes_str(file_path: str):
 
 
 def _return_img_carousel_files() -> list:
-    images_path = Path(Path(__file__).parent, '.data', 'png') 
+    images_path = Path(Path(__file__).parent, '.data', 'png')
     return [
         str(pth) for pth in Path(images_path).iterdir() if Path(pth).is_file()
         ]
@@ -32,22 +32,29 @@ def _return_img_carousel_files() -> list:
 
 def _return_encoded_imgs_for_display(imgs_path: str):
     return [
-        f'data:image/png;base64,{_return_url_bytes_str(img)}' for 
+        f'data:image/png;base64,{_return_url_bytes_str(img)}' for
         img in imgs_path
         ]
 
 
 def _return_image_carousel(set_height=300) -> components:
     main_block, display_block = st.empty(), st.empty()
-    image_carousel_component = components.declare_component(
-        "image-carousel-component", 
-        path="/home/dale/dale_working_folder/mj_app/lib/python3.11/site-packages/Streamlit-Image-Carousel/frontend/public"
-        )
-    all_png_list =  _return_img_carousel_files()
+    try:
+        webapp_dir = Path(__file__).parents[1]
+        image_carousel_component = components.declare_component(
+            "image-carousel-component",
+            path=f"{webapp_dir}/lib/site-packages/Streamlit-Image-Carousel/frontend/public"
+            )
+    except st.errors.StreamlitAPIException:
+        image_carousel_component = components.declare_component(
+            "image-carousel-component",
+            path="C:/Users/dludwinski/dale_working_folder/cannabis_cult_webapp/lib/site-packages/Streamlit-Image-Carousel/frontend/public"
+            )
+    all_png_list = _return_img_carousel_files()
     png_urls = _return_encoded_imgs_for_display(all_png_list)
     with display_block:
         selected_image = image_carousel_component(
-            imageUrls=png_urls, 
+            imageUrls=png_urls,
             height=set_height,
             )
     with main_block:
@@ -56,23 +63,23 @@ def _return_image_carousel(set_height=300) -> components:
         else:
             _display_selected_img(selected_image)
 
- 
+
 def _cycle_img_display(img_url_list: list) -> None:
     for img in img_url_list:
         st.markdown(
             f"""
-            <div 
-            style="display: flex; justify-content: center; 
+            <div
+            style="display: flex; justify-content: center;
             max-height:250px; width: auto; height=auto;">
-            <img style="text-align:center;"; 
+            <img style="text-align:center;";
             src="{img}">
             </div>
             """,
             unsafe_allow_html=True,
             )
         time.sleep(7)
-    
-            
+
+
 async def _async_any_func(func, *args, **kwargs):
     _asynced = await func(*args, **kwargs)
     return _asynced
@@ -81,10 +88,10 @@ async def _async_any_func(func, *args, **kwargs):
 def _display_selected_img(img: str) -> st.image:
     return st.markdown(
         f"""
-        <div 
-        style="display: flex; justify-content: center; 
+        <div
+        style="display: flex; justify-content: center;
         max-height:250px; width:auto;">
-        <img style="text-align:center;"; 
+        <img style="text-align:center;";
         src="{img}">
         </div>
         """,
@@ -99,15 +106,15 @@ def _return_image_selector(set_key=None) -> int:
     main_block = st.empty()
     with main_block:
         selected_image = clickable_images(
-            png_urls, 
-            titles=[], 
+            png_urls,
+            titles=[],
             div_style={
-                "display": "flex", 
-                "justify-content": "center", 
+                "display": "flex",
+                "justify-content": "center",
                 "flex-wrap": "wrap"
                 },
             img_style={
-                "margin": "5px", 
+                "margin": "5px",
                 "height": "200px",
                 "text-align": "center",
                 },
@@ -118,7 +125,7 @@ def _return_image_selector(set_key=None) -> int:
 
 def _test_main():
     _return_image_carousel()
-    
+
 
 if __name__ == '__main__':
     st.set_page_config(
