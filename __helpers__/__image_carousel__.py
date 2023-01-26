389efc54ea433394b06a5bab4411af5603e6a7b9
quaaalud <86638315+quaaalud@users.noreply.github.com>
2023-01-25 21:39:47 -0600

    added auto change with response to user selection

--- __helpers__/__image_carousel__.py
+++ __helpers__/__image_carousel__.py
@@ -24,7 +24,7 @@ def _return_url_bytes_str(file_path: str):
 
 
 def _return_img_carousel_files() -> list:
-    images_path = Path(Path(__file__).parent, '.data', 'png')
+    images_path = Path(Path(__file__).parent, '.data', 'carousel_images') 
     return [
         str(pth) for pth in Path(images_path).iterdir() if Path(pth).is_file()
         ]
@@ -32,66 +32,87 @@ def _return_img_carousel_files() -> list:
 
 def _return_encoded_imgs_for_display(imgs_path: str):
     return [
-        f'data:image/png;base64,{_return_url_bytes_str(img)}' for
+        f'data:image/png;base64,{_return_url_bytes_str(img)}' for 
         img in imgs_path
         ]
 
+def _hide_loading_icon():
+    hide_icon = """
+    <style>
+    div[data-testid="stToolbar"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    div[data-testid="stDecoration"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    div[data-testid="stStatusWidget"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    </style>
+    """
+    st.markdown(hide_icon, unsafe_allow_html=True)
 
-def _return_image_carousel(set_height=300) -> components:
+
+async def _return_image_carousel(set_height=300) -> components:
+    _hide_loading_icon()
     main_block, display_block = st.empty(), st.empty()
-    try:
-        webapp_dir = Path(__file__).parents[1]
-        image_carousel_component = components.declare_component(
-            "image-carousel-component",
-            path=f"{webapp_dir}/lib/site-packages/Streamlit-Image-Carousel/frontend/public"
-            )
-    except st.errors.StreamlitAPIException:
-        image_carousel_component = components.declare_component(
-            "image-carousel-component",
-            path="C:/Users/dludwinski/dale_working_folder/cannabis_cult_webapp/lib/site-packages/Streamlit-Image-Carousel/frontend/public"
-            )
-    all_png_list = _return_img_carousel_files()
+    image_carousel_component = components.declare_component(
+        "image-carousel-component", 
+        path="/home/dale/dale_working_folder/mj_app/lib/python3.11/site-packages/Streamlit-Image-Carousel/frontend/public"
+        )
+    all_png_list =  _return_img_carousel_files()
     png_urls = _return_encoded_imgs_for_display(all_png_list)
     with display_block:
         selected_image = image_carousel_component(
-            imageUrls=png_urls,
-            height=set_height,
+            imageUrls=png_urls, 
+            height=set_height
             )
     with main_block:
-        if selected_image is None:
-            _cycle_img_display(png_urls)
+        selected_index = _return_index_of_list_item(png_urls, selected_image)
+        if selected_image:
+            await _display_selected_img(selected_image)
         else:
-            _display_selected_img(selected_image)
+            while True:
+                await _display_selected_img(png_urls[selected_index])
+                await sleep_async(4)
+                selected_index = (selected_index + 1) % len(png_urls)
 
 
-def _cycle_img_display(img_url_list: list) -> None:
-    for img in img_url_list:
-        st.markdown(
-            f"""
-            <div
-            style="display: flex; justify-content: center;
-            max-height:250px; width: auto; height=auto;">
-            <img style="text-align:center;";
-            src="{img}">
-            </div>
-            """,
-            unsafe_allow_html=True,
-            )
-        time.sleep(7)
+def _get_indexed_list_dict(any_list: list) -> dict:
+    return {index: item for index, item in enumerate(any_list)}            
 
 
-async def _async_any_func(func, *args, **kwargs):
-    _asynced = await func(*args, **kwargs)
-    return _asynced
+def _return_index_of_list_item(any_list: list, item) -> int:
+    if item in any_list:
+        temp_dict = {item: index for index, item in enumerate(any_list)}   
+        return temp_dict[item]
+    else:
+        return 0
 
 
-def _display_selected_img(img: str) -> st.image:
+async def wait_for_function(func, *args, **kwargs):
+    async_task = asyncio.create_task(func(*args, **kwargs))
+    async_result = await async_task
+    return async_result
+
+    
+async def sleep_async(seconds):
+    await asyncio.sleep(seconds)
+
+
+async def _display_selected_img(img) -> st.image:
     return st.markdown(
         f"""
-        <div
-        style="display: flex; justify-content: center;
-        max-height:250px; width:auto;">
-        <img style="text-align:center;";
+        <div 
+        style="display: flex; justify-content: center; 
+        max-height:250px;">
+        <img style="text-align:center; width:auto"; 
         src="{img}">
         </div>
         """,
@@ -106,15 +127,15 @@ def _return_image_selector(set_key=None) -> int:
     main_block = st.empty()
     with main_block:
         selected_image = clickable_images(
-            png_urls,
-            titles=[],
+            png_urls, 
+            titles=[], 
             div_style={
-                "display": "flex",
-                "justify-content": "center",
+                "display": "flex", 
+                "justify-content": "center", 
                 "flex-wrap": "wrap"
                 },
             img_style={
-                "margin": "5px",
+                "margin": "5px", 
                 "height": "200px",
                 "text-align": "center",
                 },
@@ -122,9 +143,6 @@ def _return_image_selector(set_key=None) -> int:
             )
     if selected_image >= 0:
         pass
-
-def _test_main():
-    _return_image_carousel()
 
 
 if __name__ == '__main__':
@@ -134,4 +152,4 @@ if __name__ == '__main__':
         menu_items=None,
         initial_sidebar_state='collapsed',
     )
-    _test_main()
+    asyncio.run(_return_image_carousel())
