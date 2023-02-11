fa467c9080d0bcaf8313368607764465e6634e69
quaaalud <dludwins@outlook.com>
2023-02-11 17:20:55 -0600

    added shopify link

--- Home.py
+++ Home.py
@@ -42,6 +42,7 @@ def mj_app_display() -> None:
     import __add_pages_links__
     import __sm_links__
     import get_video_to_display
+    import __add_shopify_link__
     from __add_background_from_local__ import add_bg_from_local
     import shuffle_list
     # Hide mainmenu and footer
@@ -66,12 +67,11 @@ def mj_app_display() -> None:
     with logo_col:
         _return_cc_title()
     st.markdown('<div><br></div>', unsafe_allow_html=True)
-    # expander was here when video was on the page
+    e1, vid_col, e2 = st.columns([1.5, 7, 1.5])
     carousel_container = st.container()
     st.markdown('<div><br></div>', unsafe_allow_html=True)
     daily_deals.display_daily_deals()
     st.markdown('<div><br><br></div>', unsafe_allow_html=True)
-#    e1, vid_col, e2 = st.columns([1.5, 2, 1.5])
     with st.expander('Join our mailing list', expanded=True):
         giveaway_form_capture._giveaway_form()
         st.markdown(_get_disclaimer_text(), unsafe_allow_html=True)
@@ -79,8 +79,8 @@ def mj_app_display() -> None:
     __sm_links__._display_sm_links()
     __add_pages_links__._display_pages_links()
 #    vid_bytes, vid_type = get_video_to_display._display_current_video()
-#    with vid_col:
-#        st.video(vid_bytes)
+    with vid_col:
+        __add_shopify_link__._add_shopify_merch_link()
     with carousel_container:
         asyncio.run(
             __image_carousel__._return_image_carousel(
---
+++ __helpers__/__add_shopify_link__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 16:50:38 2023
+
+@author: dale
+"""
+
+import streamlit as st
+from local_image_to_html_with_href import _clickable_image_without_caption
+from pathlib import Path
+
+
+def _add_shopify_merch_link() -> str:
+    shopify_link = 'https://thecannabiscult.myshopify.com/'
+    shopify_img = str(
+        Path(
+            Path(__file__).parent, '.data', 'cc_logos', 'cc_cult_hoodie.png'
+        )
+    )
+    html_str = f"""
+    <div>
+    <h1 style="text-align: center;">
+    <a href="{shopify_link}" style="color: #301934;">
+    First edition merchandise now available!
+    </a>
+    </h1>
+    </div>
+    """
+    shopify_container = st.container()
+    with shopify_container:
+        st.markdown(html_str, unsafe_allow_html=True)
+        st.markdown(
+            _clickable_image_without_caption(shopify_link, shopify_img),
+            unsafe_allow_html=True,
+        )
